06936109fa200ff41d7f1fad7663529707d5f933
Bobby Brennan <bobby.brennan@gmail.com>
2015-05-09 22:47:20 +0000

    Start principia work

---
+++ convert-principia.sh
@@ -0,0 +1,6 @@
+for i in `seq 0 718`;
+do
+  echo "Converting page " $i
+  convert -density 300 pdf/principia-1.pdf[$i] -depth 8 img/v1-p$i.tiff
+  tesseract -l eng+pcm img/v1-p$i.tiff txt/v1-p$i
+done
